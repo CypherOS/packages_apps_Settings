6734b8fffdfe11397966755151ac124da4561213
Lars Greiss <kufikugel@googlemail.com>
2014-11-15 13:43:09 +0100

    Settings: add safe headset volume and less annoying sound notification
    Add settings for both features.
    
    Screw'd:
    Also add some general helper strings
    
    Change-Id: I8ab4f4b9ec320238b05ac51fb76ac24b93c37d37
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/InstrumentedFragment.java
+++ src/com/android/settings/InstrumentedFragment.java
@@ -28,6 +28,7 @@ public abstract class InstrumentedFragment extends PreferenceFragment {
     // Declare new temporary categories here, starting after this value.
     public static final int UNDECLARED = 100000;
 	public static final int ROTATION = 100001;
+    public static final int ADVANCED = 100002;
 
     // Used by PreferenceActivity for the dummy fragment it adds, no useful data here.
     public static final int PREFERENCE_ACTIVITY_FRAGMENT = UNDECLARED + 1;
