da1d4ebaddf56e19b53e934c0956e93c16cc1389
George G <kreach3r@users.noreply.github.com>
2015-11-03 23:57:22 +0200

    DayNight: improve storage summary
    PS1:
    * Fix storage summary colors in Settings
    * Use an attribute, so it reverts automatically when using Day/Night (check it with the default theme)
    * Correctly colors the eject button, too (if you use xml, you don't get that)
    * Code example for using attributes in code﻿
    
    PS2: NO TABS!
    
    Change-Id: I69ad3c94a508a4ba54e0f437406e08ed986eb61f

--- src/com/android/settings/deviceinfo/StorageVolumePreference.java
+++ src/com/android/settings/deviceinfo/StorageVolumePreference.java
@@ -56,6 +56,10 @@ public class StorageVolumePreference extends Preference {
         mStorageManager = context.getSystemService(StorageManager.class);
         mVolume = volume;
         mColor = color;
+        TypedValue typedValue = new TypedValue();
+
+        context.getTheme().resolveAttribute(android.R.attr.textColorSecondary, typedValue, true);
+        mSecondaryColor = context.getResources().getColor(typedValue.resourceId);
 
         TypedValue typedValue = new TypedValue();
         context.getTheme().resolveAttribute(android.R.attr.textColorSecondary, typedValue, true);
