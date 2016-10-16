ee364e2e1b363c375bc0d3d8d330057ba06ba85b
Chris <chriscrawf95@gmail.com>
2016-10-16 01:04:42 -0700

    Settings: Forward port Software information
    - Add back BuildNotes
    - Add back social links for the website and g+

---
+++ src/com/android/settings/cypher/deviceinfo/BuildNotes.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2016 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cypher.deviceinfo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BuildNotes extends Fragment {
+
+    private static final String CHANGELOG_PATH = "/system/etc/Changelog.txt";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+        InputStreamReader inputReader = null;
+        String text = null;
+
+        try {
+            StringBuilder data = new StringBuilder();
+            char tmp[] = new char[2048];
+            int numRead;
+
+            inputReader = new FileReader(CHANGELOG_PATH);
+            while ((numRead = inputReader.read(tmp)) >= 0) {
+                data.append(tmp, 0, numRead);
+            }
+            text = data.toString();
+        } catch (IOException e) {
+            text = getString(R.string.buildnotes_settings_error);
+        } finally {
+            try {
+                if (inputReader != null) {
+                    inputReader.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+
+        final TextView textView = new TextView(getActivity());
+        textView.setText(text);
+
+        final ScrollView scrollView = new ScrollView(getActivity());
+        scrollView.addView(textView);
+
+        return scrollView;
+    }
+}/*
+ * Copyright (C) 2016 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cypher.deviceinfo;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.dashboard.SummaryLoader;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import com.android.settings.R;
+import com.android.settings.SettingsActivity;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SoftwareInfo extends SettingsPreferenceFragment implements
+        Indexable {
+    private static final String TAG = "SoftwareInfo";
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.software_info);
+		
+        final Activity activity = getActivity();
+        final ContentResolver resolver = activity.getContentResolver();
+		
+	}
+	
+	@Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.ADDITIONS;
+    }
+	
+	public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.software_info;
+                    result.add(sir);
+
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> result = new ArrayList<String>();
+                    return result;
+                }
+            };
+}
--- src/com/android/settings/search/SearchIndexableResources.java
+++ src/com/android/settings/search/SearchIndexableResources.java
@@ -19,6 +19,7 @@ package com.android.settings.search;
 import android.provider.SearchIndexableResource;
 import com.android.settings.cypher.Additions;
 import com.android.settings.cypher.Gestures;
+import com.android.settings.cypher.deviceinfo.SoftwareInfo;
 import com.android.settings.DateTimeSettings;
 import com.android.settings.DevelopmentSettings;
 import com.android.settings.DeviceInfoSettings;
@@ -165,6 +166,13 @@ public final class SearchIndexableResources {
                         NO_DATA_RES_ID,
                         Gestures.class.getName(),
                         R.drawable.ic_settings_gestures));
+						
+		sResMap.put(SoftwareInfo.class.getName(),
+                new SearchIndexableResource(
+                        Ranking.getRankForClassName(SoftwareInfo.class.getName()),
+                        NO_DATA_RES_ID,
+                        SoftwareInfo.class.getName(),
+                        R.drawable.ic_settings_about));
 
         sResMap.put(SoundSettings.class.getName(),
                 new SearchIndexableResource(
