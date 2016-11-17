262c514f30040d6d683109592066fca7df6e7220
Chris <chriscrawf95@gmail.com>
2016-11-17 18:13:19 -0500

    BatterySettings: Add missing import for PreferenceScreen
    Change-Id: Iad3e38bfbb3ae2b36a467e84875b198b058554af

--- src/com/android/settings/cypher/fragments/BatterySettings.java
+++ src/com/android/settings/cypher/fragments/BatterySettings.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceCategory;
