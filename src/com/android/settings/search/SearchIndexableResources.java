97b05fd93faebb573c36a946fcd9a2b464f92788
Chris <chriscrawf95@gmail.com>
2017-01-13 23:52:51 -0500

    Settings: Forward Statusbar Settings and external features
    The first set of features are back for aoscp! This commit reintroduces
    the classic statusbar settings with clock and date customizations to top
    it off.
    
    - Add back statusbar settings
    - Make statusbar settings searchable
    - Statusbar clock positions
    - Statusbar AM/PM customizations
    - Statusbar date visibility
    - Statubar date position
    - Statusbar date format
    - New statusbar icon
    - Modfiied some strings to fit our standards
    - Added Quick Pulldown
    - Added Smart Pulldown
    - Added Network Traffic
    - Also add Navbar Tuner preference to Display Settings
    
    This commit was referenced from:
    AOSIP/platform_packages_apps_Settings@ec9b0bc
    and an array of commits located in
    AOSIP/platform_packages_apps_Settings, Commit's were also referenced
    from our old Settings Package located here
    Sirhc95@packages_apps_Settings with each commit having proper authorship
    linked

--- src/com/android/settings/search/SearchIndexableResources.java
+++ src/com/android/settings/search/SearchIndexableResources.java
@@ -19,6 +19,7 @@ package com.android.settings.search;
 import android.provider.SearchIndexableResource;
 import com.android.settings.aoscp.Additions;
 import com.android.settings.aoscp.deviceinfo.SoftwareInfo;
+import com.android.settings.aoscp.fragments.StatusbarSettings;
 import com.android.settings.aoscp.fragments.SystemSettings;
 import com.android.settings.DateTimeSettings;
 import com.android.settings.DevelopmentSettings;
@@ -168,6 +169,13 @@ public final class SearchIndexableResources {
                         NO_DATA_RES_ID,
                         SoftwareInfo.class.getName(),
                         R.drawable.ic_settings_about));
+						
+		sResMap.put(StatusbarSettings.class.getName(),
+                new SearchIndexableResource(
+                        Ranking.getRankForClassName(StatusbarSettings.class.getName()),
+                        NO_DATA_RES_ID,
+                        StatusbarSettings.class.getName(),
+                        R.drawable.ic_settings_statusbar));
 
 		sResMap.put(SystemSettings.class.getName(),
                 new SearchIndexableResource(
