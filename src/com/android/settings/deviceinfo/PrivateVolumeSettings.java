4b9de32370ac5778b5a9c062cf49651c07dc39c6
Chris <chriscrawf95@gmail.com>
2017-02-05 23:54:40 -0500

    AdvancedStorage: UX changes for simplified experience
    Instead of hiding this feature in the 3 Dot menu, let's
    make it it's own action with a custom icon indicating the
    user that it exist.
    
    USB Unlocked Data Access is cool, but appeals to the enthusiasts.
    Let's make it more simple to a common user by rebranding this to
    USB Connection.
    
    Change-Id: I3254f4befc47716bfd2abd1b8b2e574294646959

--- src/com/android/settings/deviceinfo/PrivateVolumeSettings.java
+++ src/com/android/settings/deviceinfo/PrivateVolumeSettings.java
@@ -388,7 +388,11 @@ public class PrivateVolumeSettings extends SettingsPreferenceFragment {
 
     @Override
       public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-          menu.add(Menu.NONE, MENU_ADVANCED, Menu.NONE, R.string.storage_menu_advanced);
+          
+		  menu.add(Menu.NONE, MENU_ADVANCED, Menu.NONE, R.string.storage_menu_advanced)
+  		      .setIcon(R.drawable.ic_menu_connection)
+ 			  .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		  
           inflater.inflate(R.menu.storage_volume, menu);
           super.onCreateOptionsMenu(menu, inflater);
       }
