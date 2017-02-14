6e6750b23fe44e782b55632e18fedbfbf74e7231
Chris <chriscrawf95@gmail.com>
2017-02-14 09:55:48 -0500

    PrivateVolumeSettings: Rework and cleanup
    Removes the explore preference, since we utilize this in our
    files apps, and clicking on each catergory will present this
    layout anyways. Also remove storage cleaner as a menu item
    since this is available in the storage manager.
    
    - Small maintenence: Adjust summary loader string, set to "Version"
    instead of "CypherOS"
    
    Change-Id: Ib73d345d42a184714ef4d63ab2b7674094d82fdb

--- src/com/android/settings/deviceinfo/PrivateVolumeSettings.java
+++ src/com/android/settings/deviceinfo/PrivateVolumeSettings.java
@@ -131,7 +131,6 @@ public class PrivateVolumeSettings extends SettingsPreferenceFragment {
     private int mHeaderPoolIndex;
     private int mItemPoolIndex;
 
-    private Preference mExplore;
     private Preference mAutomaticStorageManagement;
 
     private boolean mNeedsUpdate;
@@ -190,7 +189,6 @@ public class PrivateVolumeSettings extends SettingsPreferenceFragment {
         mSummary = new StorageSummaryPreference(getPrefContext());
         mCurrentUser = mUserManager.getUserInfo(UserHandle.myUserId());
 
-        mExplore = buildAction(R.string.storage_menu_explore);
         mAutomaticStorageManagement = buildAction(R.string.storage_menu_manage);
 
         mNeedsUpdate = true;
@@ -256,10 +254,6 @@ public class PrivateVolumeSettings extends SettingsPreferenceFragment {
         }
 
         addItem(screen, R.string.storage_detail_cached, null, UserHandle.USER_NULL);
-
-        if (showShared) {
-            addPreference(screen, mExplore);
-        }
 
         final long freeBytes = mVolume.getPath().getFreeSpace();
         final long usedBytes = mTotalSize - freeBytes;
@@ -392,8 +386,7 @@ public class PrivateVolumeSettings extends SettingsPreferenceFragment {
 		  menu.add(Menu.NONE, MENU_ADVANCED, Menu.NONE, R.string.storage_menu_advanced)
   		      .setIcon(R.drawable.ic_menu_connection)
  			  .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
-		  
-          inflater.inflate(R.menu.storage_volume, menu);
+		
           super.onCreateOptionsMenu(menu, inflater);
       }
 
@@ -406,7 +399,6 @@ public class PrivateVolumeSettings extends SettingsPreferenceFragment {
         final MenuItem unmount = menu.findItem(R.id.storage_unmount);
         final MenuItem format = menu.findItem(R.id.storage_format);
         final MenuItem migrate = menu.findItem(R.id.storage_migrate);
-		final MenuItem manage = menu.findItem(R.id.storage_free);
 
         // Actions live in menu for non-internal private volumes; they're shown
         // as preference items for public volumes.
@@ -415,14 +407,11 @@ public class PrivateVolumeSettings extends SettingsPreferenceFragment {
             mount.setVisible(false);
             unmount.setVisible(false);
             format.setVisible(false);
-			manage.setVisible(getResources().getBoolean(
-                    R.bool.config_storage_manager_settings_enabled));
         } else {
             rename.setVisible(mVolume.getType() == VolumeInfo.TYPE_PRIVATE);
             mount.setVisible(mVolume.getState() == VolumeInfo.STATE_UNMOUNTED);
             unmount.setVisible(mVolume.isMountedReadable());
             format.setVisible(true);
-			manage.setVisible(false);
         }
 
         format.setTitle(R.string.storage_menu_format_public);
@@ -466,11 +455,6 @@ public class PrivateVolumeSettings extends SettingsPreferenceFragment {
                 final Intent intent = new Intent(context, StorageWizardMigrateConfirm.class);
                 intent.putExtra(VolumeInfo.EXTRA_VOLUME_ID, mVolume.getId());
                 startActivity(intent);
-                return true;
-		    case R.id.storage_free:
-                final Intent deletion_helper_intent =
-                        new Intent(StorageManager.ACTION_MANAGE_STORAGE);
-                startActivity(deletion_helper_intent);
                 return true;
         }
         return super.onOptionsItemSelected(item);
