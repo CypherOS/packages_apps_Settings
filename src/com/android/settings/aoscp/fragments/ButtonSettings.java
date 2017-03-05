c19240bf4a1a377b039dd41b84899b70366ea6a1
Chris <chris@cypheros.co>
2017-03-05 17:05:41 -0500

    Settings: Import PreferenceGroup
    Needed for intent activity detection
    
    Change-Id: I8cb22f9159871d3db72308d9b21bdc6d4496a007

--- src/com/android/settings/aoscp/fragments/ButtonSettings.java
+++ src/com/android/settings/aoscp/fragments/ButtonSettings.java
@@ -31,6 +31,7 @@ import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceGroup;
 import android.support.v7.preference.PreferenceScreen;
 import android.util.Log;
 import android.view.IWindowManager;
