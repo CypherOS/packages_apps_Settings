09a214109f2b423aa84fc2e2da5208192260765c
Chris <chris@cypheros.co>
2017-04-12 21:43:39 -0400

    Introduce Support Manager Interface
    This adds a support manager on the settings dashboard in the form
    of a slidingtab. Users can swipe to the secondary dashboard which
    will provide software based support such as feature locations,
    tips and tricks, information about our software and a dedicated
    bug reporting option.
    
    Our support manager is based on Google's support feature, introduced
    in nougat for Pixel Devices. The infrastructure is similar, but we
    don't offer chat and phone support, so let's make use of this with for
    something we offer.
    
    DO NOT MERGE: Google's support interface is based on a feature overlay,
    that
    must be enabled properly. We intentionally stripped parts from it and
    combined
    it with our own implementation. First objective is to confirm this shows
    as
    a proper slidingtab in the dashboard, then we continue with layout
    construction.
    
    PS3: Rebase Support Manager on the latest AOSP changes. PS1 was heavily
    outdated
    from last year
    
    Change-Id: Ic09214c81b1f91d938e4e52eedc04841a39e0612

---
+++ src/com/android/settings/aoscp/support/SupportManager.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2017 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.aoscp.support;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.internal.logging.MetricsProto;
+import com.android.settings.InstrumentedFragment;
+import com.android.settings.aoscp.support.SupportManagerCallback;
+import com.android.settings.aoscp.support.SupportManagerItemAdapter;
+import com.android.settings.overlay.FeatureFactory;
+import com.android.settings.R;
+
+/**
+ * Fragment for support tab in aoscp settings.
+ */
+public final class SupportManager extends InstrumentedFragment implements View.OnClickListener {
+
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
+    
+    private Activity mActivity;
+    private View mContent;
+    private RecyclerView mRecyclerView;
+	private SupportManagerCallback mSupportManagerCallback;
+    private SupportManagerItemAdapter mSupportManagerItemAdapter;
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.SUPPORT_FRAGMENT;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = getActivity();
+		mSupportManagerCallback =
+                FeatureFactory.getFactory(mActivity).getSupportManagerCallback(mActivity);
+        mSupportManagerItemAdapter = new SupportManagerItemAdapter(mActivity, savedInstanceState,
+                mSupportManagerCallback, this /* itemClickListener */);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        mContent = inflater.inflate(R.layout.support_manager, container, false);
+        mRecyclerView = (RecyclerView) mContent.findViewById(R.id.support_manager_items);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(
+                getActivity(), LinearLayoutManager.VERTICAL, false /* reverseLayout */));
+        mRecyclerView.setAdapter(mSupportManagerItemAdapter);
+        return mContent;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+	
+	@Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mSupportManagerItemAdapter.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final SupportManagerItemAdapter.ViewHolder vh =
+                (SupportManagerItemAdapter.ViewHolder) mRecyclerView.getChildViewHolder(v);
+        mSupportManagerItemAdapter.onItemClicked(vh.getAdapterPosition());
+    }
+}
