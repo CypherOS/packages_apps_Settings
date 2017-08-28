d536e9a2446ac2508562d7810fb490e8c6e98fac
Chris <chris@cypheros.co>
2017-08-28 02:56:03 -0400

    [WIP]Settings: add upgraded ambient display user interface
    This new user interface is based on the new "MasterSwitchPreference"
    API. This allows Ambient Display to be toggled on/off directly from
    it's parent class, with the addition of opening a full menu that
    contains Ambient Display based features (such as lift to check phone,
    double tap to check phone etc.)
    
    Leaving ambient display preferences separate from it's parent doesn't
    seem ideal and we would rather keep it in one place, to give users
    a better indication of what said feature depends on.
    
    Change-Id: I5a5e4266a27fe2f782276215948f02465c862e0b

--- src/com/android/settings/display/DozePreferenceController.java
+++ src/com/android/settings/display/DozePreferenceController.java
@@ -14,35 +14,51 @@
 package com.android.settings.display;
 
 import android.content.Context;
+import android.database.ContentObserver;
 import android.os.Build;
+import android.os.Handler;
 import android.os.SystemProperties;
 import android.provider.Settings;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
 import android.text.TextUtils;
 
+import com.android.settings.R;
 import com.android.settings.core.PreferenceController;
 import com.android.settings.core.instrumentation.MetricsFeatureProvider;
+import com.android.settings.core.lifecycle.Lifecycle;
+import com.android.settings.core.lifecycle.LifecycleObserver;
+import com.android.settings.core.lifecycle.events.OnStart;
+import com.android.settings.core.lifecycle.events.OnStop;
 import com.android.settings.overlay.FeatureFactory;
+import com.android.settings.widget.MasterSwitchPreference;
 
 import static android.provider.Settings.Secure.DOZE_ENABLED;
 import static com.android.internal.logging.nano.MetricsProto.MetricsEvent.ACTION_AMBIENT_DISPLAY;
 
 public class DozePreferenceController extends PreferenceController implements
-        Preference.OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener, LifecycleObserver, OnStart, OnStop {
 
     private static final String KEY_DOZE = "doze";
 
+	private MasterSwitchPreference mDozePref;
     private final MetricsFeatureProvider mMetricsFeatureProvider;
 
-    public DozePreferenceController(Context context) {
+    public DozePreferenceController(Context context, Lifecycle lifecycle) {
         super(context);
+		lifecycle.addObserver(this);
         mMetricsFeatureProvider = FeatureFactory.getFactory(context).getMetricsFeatureProvider();
     }
-
+	
     @Override
     public String getPreferenceKey() {
         return KEY_DOZE;
+    }
+	
+	@Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mDozePref = (MasterSwitchPreference) screen.findPreference(KEY_DOZE);
     }
 
     @Override
@@ -56,14 +72,49 @@ public class DozePreferenceController extends PreferenceController implements
     @Override
     public void updateState(Preference preference) {
         int value = Settings.Secure.getInt(mContext.getContentResolver(), DOZE_ENABLED, 1);
-        ((SwitchPreference) preference).setChecked(value != 0);
+        ((MasterSwitchPreference) preference).setChecked(value != 0);
+		updateSummary();
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         boolean value = (Boolean) newValue;
         Settings.Secure.putInt(mContext.getContentResolver(), DOZE_ENABLED, value ? 1 : 0);
+		updateSummary();
         return true;
+    }
+
+	@Override
+    public void onStart() {
+        mContext.getContentResolver().registerContentObserver(
+                Settings.Secure.getUriFor(Settings.Secure.DOZE_PULSE_ON_PICK_UP)
+                , true, mObserver);
+    }
+
+    @Override
+    public void onStop() {
+        mContext.getContentResolver().unregisterContentObserver(mObserver);
+    }
+	
+	private void updateSummary() {
+        final boolean state = isDozeActivated();
+        final int register = state ? R.string.ambient_display_settings_on_summary
+                : R.string.ambient_display_settings_off_summary;
+        final String summary = mContext.getString(register);
+
+        mDozePref.setSummary(summary);
+    }
+	
+	private final ContentObserver mObserver = new ContentObserver(new Handler()) {
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSummary();
+        }
+    };
+	
+	public boolean isDozeActivated() {
+        Settings.Secure.getInt(mContext.getContentResolver(), DOZE_ENABLED, 1);
+		return true;
     }
 
     @Override
