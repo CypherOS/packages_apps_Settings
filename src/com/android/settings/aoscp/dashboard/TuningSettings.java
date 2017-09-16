9342f362e8bfb1a50ff1fabeb03b3578d76c25bb
Chris <chris@cypheros.co>
2017-09-15 23:49:09 -0400

    Tuning: add screenshot mode setting
    Adds a custom list preference to select screenshot modes.
    
    - Forward ported from Nougat and updated for Oreo
    
    Change-Id: I726dc5b663fa3f2c09f4e36b7b0d873c71a9ecf3

--- src/com/android/settings/aoscp/dashboard/TuningSettings.java
+++ src/com/android/settings/aoscp/dashboard/TuningSettings.java
@@ -25,6 +25,7 @@ import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.aoscp.tuning.ScreenshotModePreferenceController;
 import com.android.settings.core.PreferenceController;
 import com.android.settings.core.lifecycle.Lifecycle;
 import com.android.settings.dashboard.DashboardFragment;
@@ -41,6 +42,8 @@ public class TuningSettings extends DashboardFragment implements Indexable {
 
     private static final String LOG_TAG = "TuningSettings";
 
+    private static final String KEY_SCREENSHOT_TYPE = "screenshot_type";
+    
     private final FooterPreferenceMixin mFooterPreferenceMixin =
             new FooterPreferenceMixin(this, getLifecycle());
 
@@ -79,6 +82,7 @@ public class TuningSettings extends DashboardFragment implements Indexable {
     private static List<PreferenceController> buildPreferenceControllers(Context context,
             Activity activity, Fragment fragment, Lifecycle lifecycle) {
         final List<PreferenceController> controllers = new ArrayList<>();
+        controllers.add(new ScreenshotModePreferenceController(context, KEY_SCREENSHOT_TYPE));
         return controllers;
     }
 
