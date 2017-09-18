f8f8248ad023ac60893ff64ee5cc8f4f869b913c
Chris <chris@cypheros.co>
2017-09-18 01:16:38 -0400

    Settings: add button customization features
    Forward ports all button customization features from our nougat releases
    
    Change-Id: Id2025fe37f110f683ff33c891614260529516ea6

--- src/com/android/settings/aoscp/dashboard/ButtonSettings.java
+++ src/com/android/settings/aoscp/dashboard/ButtonSettings.java
@@ -25,6 +25,9 @@ import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.aoscp.buttons.ButtonBrightnessPreferenceController;
+import com.android.settings.aoscp.buttons.NavigationBarPreferenceController;
+import com.android.settings.aoscp.buttons.SwapNavigationKeysPreferenceController;
 import com.android.settings.core.PreferenceController;
 import com.android.settings.core.lifecycle.Lifecycle;
 import com.android.settings.dashboard.DashboardFragment;
@@ -40,6 +43,10 @@ import java.util.List;
 public class ButtonSettings extends DashboardFragment implements Indexable {
 
     private static final String LOG_TAG = "ButtonSettings";
+	
+	private static final String KEY_BUTTON_BRIGHTNESS = "button_brightness";
+	private static final String KEY_NAVIGATION_BAR = "navigation_bar";
+	private static final String KEY_SWAP_NAVIGATION_KEYS = "swap_navigation_keys";
     
     private final FooterPreferenceMixin mFooterPreferenceMixin =
             new FooterPreferenceMixin(this, getLifecycle());
@@ -79,6 +86,9 @@ public class ButtonSettings extends DashboardFragment implements Indexable {
     private static List<PreferenceController> buildPreferenceControllers(Context context,
             Activity activity, Fragment fragment, Lifecycle lifecycle) {
         final List<PreferenceController> controllers = new ArrayList<>();
+		controllers.add(new ButtonBrightnessPreferenceController(context, KEY_BUTTON_BRIGHTNESS));
+		controllers.add(new NavigationBarPreferenceController(context, KEY_NAVIGATION_BAR));
+		controllers.add(new SwapNavigationKeysPreferenceController(context, KEY_SWAP_NAVIGATION_KEYS));
         return controllers;
     }
 
