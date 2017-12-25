f75374a6ead1a6f66001d5e378e618d7b830ff8f
Chris <chris@cypheros.co>
2017-12-24 23:29:53 -0500

    Settings: Introduce Color Manager (ColorOM)
    No commit message yet. Just a light OMS based interface for now :)
    
    Change-Id: Ifcaa96c52b3e7ef292fd83b75a743073b9e521a9

---
+++ src/com/android/settings/aoscp/display/ThemePreferenceFragment.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.aoscp.display;
+
+import android.content.Context;
+import android.content.om.OverlayInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.RemoteException;
+import android.os.UserHandle;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.aoscp.display.themes.ThemeDefaultPreferenceController;
+import com.android.settings.aoscp.display.themes.ThemeTealPreferenceController;
+import com.android.settings.core.instrumentation.MetricsFeatureProvider;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThemePreferenceFragment extends DashboardFragment {
+
+    private static final String TAG = "ThemePreference";
+	
+	private Context mContext;
+	
+	private PackageManager mPackageManager = new mContext.getPackageManager();;
+	private ThemeOverlayManager mThemeOM;
+	
+	private static List<AbstractPreferenceController> buildPreferenceControllers(Context context,
+            Lifecycle lifecycle, MetricsFeatureProvider metricsFeatureProvider) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new ThemeDefaultPreferenceController(context));
+        controllers.add(new ThemeTealPreferenceController(context));
+        return controllers;
+    }
+	
+	@Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.theme_settings;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> getPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getLifecycle(), mMetricsFeatureProvider);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.DISPLAY;
+    }
+	
+	private boolean isChangeableOverlay(String packageName) {
+        try {
+            PackageInfo pkgMgr = mPackageManager.getPackageInfo(packageName, 0);
+            return pkgMgr != null && !pkgMgr.isStaticOverlay;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    public String getTheme() {
+        try {
+            List<OverlayInfo> infos = mThemeOM.getOverlayInfosForTarget("android",
+                    UserHandle.myUserId());
+            for (int i = 0, size = infos.size(); i < size; i++) {
+                if (infos.get(i).isEnabled() &&
+                        isChangeableOverlay(infos.get(i).packageName)) {
+                    return infos.get(i).packageName;
+                }
+            }
+        } catch (RemoteException e) {
+        }
+        return null;
+    }
+}
