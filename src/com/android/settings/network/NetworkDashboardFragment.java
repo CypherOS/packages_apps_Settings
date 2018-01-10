24258f5feecb0f223da76be5f1f2dfaa8862e1eb
Chris <chris@cypheros.co>
2018-01-09 23:10:23 -0500

    Settings: Implementation for Network Traffic Monitoring [2/2]
    Custom settings to enable/disable traffic monitoring and change its
    autohide threshold
    
    Change-Id: Iaf8bf480c2e4467ddf056c6deb1bc5a454c8ac07

--- src/com/android/settings/network/NetworkDashboardFragment.java
+++ src/com/android/settings/network/NetworkDashboardFragment.java
@@ -31,6 +31,7 @@ import android.view.MenuInflater;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
+import com.android.settings.aoscp.network.NetworkTrafficPreferenceController;
 import com.android.settings.core.instrumentation.MetricsFeatureProvider;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.dashboard.SummaryLoader;
@@ -103,6 +104,8 @@ public class NetworkDashboardFragment extends DashboardFragment implements
                 new MobileNetworkPreferenceController(context);
         final VpnPreferenceController vpnPreferenceController =
                 new VpnPreferenceController(context);
+		final NetworkTrafficPreferenceController networkTrafficPreferenceController =
+                new NetworkTrafficPreferenceController(context);
 
         if (lifecycle != null) {
             lifecycle.addObserver(airplaneModePreferenceController);
@@ -110,6 +113,7 @@ public class NetworkDashboardFragment extends DashboardFragment implements
             lifecycle.addObserver(wifiPreferenceController);
             lifecycle.addObserver(mobileNetworkPreferenceController);
             lifecycle.addObserver(vpnPreferenceController);
+			lifecycle.addObserver(networkTrafficPreferenceController);
         }
 
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
@@ -120,6 +124,7 @@ public class NetworkDashboardFragment extends DashboardFragment implements
         controllers.add(new ProxyPreferenceController(context));
         controllers.add(mobilePlanPreferenceController);
         controllers.add(wifiPreferenceController);
+		controllers.add(networkTrafficPreferenceController(context));
         return controllers;
     }
 
