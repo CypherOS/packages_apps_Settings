145aad03c0bf5d8e3351482f6731b8a4016f1824
Chris <chris@cypheros.co>
2018-02-17 02:40:35 -0500

    Settings: Statusbar clock customizations [1/2]
    Change-Id: I370cf9f9dc1d118c2a31cc5c8b08843dad382925

---
+++ src/com/android/settings/aoscp/datetime/AdvancedDateTimeSettings.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aoscp.datetime;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AdvancedDateTimeSettings extends DashboardFragment implements Indexable{
+
+    private static final String TAG = "AdvancedDateTimeSettings";
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.DATE_TIME;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.advanced_date_time_prefs;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> getPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getActivity(), this /* fragment */,
+                getLifecycle());
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context,
+            Activity activity, Fragment fragment, Lifecycle lifecycle) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new ClockSecondsPreferenceController(context));
+        controllers.add(new ClockStyleAmPmPreferenceController(context));
+        controllers.add(new ClockStylePreferenceController(context));
+        controllers.add(new DateDisplayPreferenceController(context));
+        controllers.add(new DateFormatPreferenceController(context));
+        controllers.add(new DateStylePreferenceController(context));
+		controllers.add(new ShowClockPreferenceController(context));
+        return controllers;
+    }
+
+    /**
+     * For Search.
+     */
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(
+                        Context context, boolean enabled) {
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.advanced_date_time_prefs;
+                    return Arrays.asList(sir);
+                }
+
+                @Override
+                public List<AbstractPreferenceController> getPreferenceControllers(Context context) {
+                    return buildPreferenceControllers(context, null /*activity */,
+                            null /* fragment */, null /* lifecycle */);
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+            };
+}
---
+++ src/com/android/settings/aoscp/datetime/DateStylePreferenceController.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.aoscp.datetime;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class DateStylePreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "DateStylePref";
+	private static final String CLOCK_DATE_STYLE = "clock_date_style";
+  
+    private ListPreference mDateStyle;
+
+    public DateStylePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return CLOCK_DATE_STYLE;
+    }
+
+	@Override
+    public void updateState(Preference preference) {
+        final ListPreference mDateStyle = (ListPreference) preference;
+        final Resources res = mContext.getResources();
+        if (mDateStyle != null) {
+            int dateStyleKey = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_DATE_STYLE, 0,
+                    UserHandle.USER_CURRENT);
+            String styleKey = String.valueOf(dateStyleKey);
+            mDateStyle.setValue(styleKey);
+            updateDateStyleSummary(mDateStyle, styleKey);
+			if (dateIndicatorDisabled()) {
+				((ListPreference) preference).setEnabled(false);
+			} else { 
+			    ((ListPreference) preference).setEnabled(true);
+			}
+        }
+    }
+	
+	protected boolean dateIndicatorDisabled() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.STATUSBAR_CLOCK_DATE_DISPLAY, 0) == 0;
+    }
+	
+	@Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        try {
+            String styleKey = (String) newValue;
+            Settings.System.putIntForUser(mContext.getContentResolver(), Settings.System.STATUSBAR_CLOCK_DATE_STYLE,
+                    Integer.parseInt(styleKey), UserHandle.USER_CURRENT);
+            updateDateStyleSummary((ListPreference) preference, styleKey);
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Could not set date style", e);
+        }
+        return true;
+    }
+
+    private void updateDateStyleSummary(Preference mDateStyle, String styleKey) {
+        if (styleKey != null) {
+            String[] values = mContext.getResources().getStringArray(R.array
+                    .clock_date_style_values);
+            final int summaryArrayResId = R.array.clock_date_style_entries;
+            String[] summaries = mContext.getResources().getStringArray(summaryArrayResId);
+            for (int i = 0; i < values.length; i++) {
+                if (styleKey.equals(values[i])) {
+                    if (i < summaries.length) {
+                        mDateStyle.setSummary(summaries[i]);
+                        return;
+                    }
+                }
+            }
+        }
+
+        mDateStyle.setSummary("");
+        Log.e(TAG, "Invalid date style value: " + styleKey);
+    }
+}
