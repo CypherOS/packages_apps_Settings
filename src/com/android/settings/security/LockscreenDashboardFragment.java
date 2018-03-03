8265661fdc8a605863cb6d582e5084b26cf1f25d
Chris <chris@cypheros.co>
2018-03-03 09:13:51 -0500

    Introduce Ambient Weather [1/2]
    Allows displaying weather information on the lockscreen and when dozing.
    
    Change-Id: Ib4421b73479152b1d623b0204bd225dcf56a5e87

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -23,6 +23,7 @@ import android.support.annotation.VisibleForTesting;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.accounts.AddUserWhenLockedPreferenceController;
+import com.android.settings.aoscp.security.AmbientWeatherPreferenceController;
 import com.android.settings.aoscp.security.AutoFaceUnlockPreferenceController;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.notification.LockScreenNotificationPreferenceController;
@@ -92,6 +93,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
             new OwnerInfoPreferenceController(context, this, lifecycle);
         controllers.add(mOwnerInfoPreferenceController);
         controllers.add(new AutoFaceUnlockPreferenceController(context));
+        controllers.add(new AmbientWeatherPreferenceController(context));
         return controllers;
     }
 
