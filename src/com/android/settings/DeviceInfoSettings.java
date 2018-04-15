160b9ea1a07d58167aab42a24f60b76385f60572
Chris <chris@cypheros.co>
2018-04-15 16:38:12 -0400

    test
    Change-Id: Id5e7005a00a5cf088ab3573cad7662f9c90cd0a3

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -20,6 +20,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.provider.SearchIndexableResource;
 import android.os.Build;
 import android.os.SystemClock;
@@ -120,6 +121,15 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         }
         final ImageView icon = (ImageView) mHeaderLayoutPref
                 .findViewById(R.id.header_icon);
+		icon.post(new Runnable() {
+			@Override
+			public void run() {
+				AnimationDrawable anim = (AnimationDrawable) icon.getDrawable();
+				if (!anim.isRunning()) {
+					anim.start();
+				}
+			}
+		});
         icon.setClickable(true);
         icon.setOnClickListener(new View.OnClickListener() {
             @Override
