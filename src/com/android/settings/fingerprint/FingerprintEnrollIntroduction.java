91dff7c54cbf825ef32745a484ef3d274ee1cf5d
Chris <chris@cypheros.co>
2018-05-08 10:22:07 -0400

    Improve Fingerprint Enroll UI
    Change-Id: I2489006d5550395890e6b38a55b00de950ffe695

--- src/com/android/settings/fingerprint/FingerprintEnrollIntroduction.java
+++ src/com/android/settings/fingerprint/FingerprintEnrollIntroduction.java
@@ -31,6 +31,7 @@ import android.widget.TextView;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.Utils;
+import com.android.settings.aoscp.fingerprint.FingerprintIntroductionAnimation;
 import com.android.settings.password.ChooseLockGeneric;
 import com.android.settings.password.ChooseLockSettingsHelper;
 import com.android.settingslib.HelpUtils;
@@ -53,6 +54,8 @@ public class FingerprintEnrollIntroduction extends FingerprintEnrollBase
     private boolean mHasPassword;
     private boolean mFingerprintUnlockDisabledByAdmin;
     private TextView mErrorText;
+	
+	private FingerprintIntroductionAnimation mAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,10 +75,25 @@ public class FingerprintEnrollIntroduction extends FingerprintEnrollBase
         cancelButton.setOnClickListener(this);
 
         mErrorText = (TextView) findViewById(R.id.error_text);
+		
+		View animationView = findViewById(R.id.introduction);
+        if (animationView instanceof FingerprintIntroductionAnimation) {
+            mAnimation = (FingerprintIntroductionAnimation) animationView;
+        } else {
+            mAnimation = null;
+        }
 
         mUserManager = UserManager.get(this);
         updatePasswordQuality();
     }
+	
+	@Override
+    protected void onStart() {
+        super.onStart();
+        if (mAnimation != null) {
+            mAnimation.start();
+		}
+	}
 
     @Override
     protected void onResume() {
@@ -100,6 +118,22 @@ public class FingerprintEnrollIntroduction extends FingerprintEnrollBase
         } else {
             mErrorText.setText(errorMsg);
             getNextButton().setVisibility(View.GONE);
+        }
+    }
+	
+	@Override
+    protected void onStop() {
+        super.onStop();
+        if (mAnimation != null) {
+            mAnimation.pause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mAnimation != null) {
+            mAnimation.stop();
         }
     }
 
