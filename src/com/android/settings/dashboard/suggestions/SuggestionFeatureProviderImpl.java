418ceb2242ad64a0abc6b24af616c8688e299af3
Chris <chris@cypheros.co>
2018-10-17 00:57:43 -0400

    Settings: Fallback to platform SettingsIntelligence
    Google's version has issues with themes and such. We can modify
    the source version instead
    
    This reverts commit d824b41e6c52aca0fc06c8b84c7b484827305cab.
    
    Change-Id: I9686a0c585beae4836fe77d7befd35c24b3814dc

--- src/com/android/settings/dashboard/suggestions/SuggestionFeatureProviderImpl.java
+++ src/com/android/settings/dashboard/suggestions/SuggestionFeatureProviderImpl.java
@@ -63,8 +63,8 @@ public class SuggestionFeatureProviderImpl implements SuggestionFeatureProvider 
     @Override
     public ComponentName getSuggestionServiceComponent() {
         return new ComponentName(
-                "com.google.android.settings.intelligence",
-                "com.google.android.settings.intelligence.modules.suggestions.SuggestionService");
+                "com.android.settings.intelligence",
+                "com.android.settings.intelligence.suggestions.SuggestionService");
     }
 
     @Override
