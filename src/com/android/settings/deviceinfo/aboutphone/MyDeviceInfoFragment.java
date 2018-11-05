88bdcf0ea896daa4b447da2ded7b27d0d6b2fd86
Chris <chris@cypheros.co>
2018-11-05 09:47:06 -0500

    deviceinfo: Set the summary to match device name
    By default, the device name controller loads in the devices model. When
    a user changes the device name, it updates in about phone but not the
    summary of about phone.
    
    To address this issue, summary loader should listen for the device name
    class member to show the updated summary
    
    Change-Id: I872a93af9bc467b7e034f8a87c478afabb87e5ed

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -27,6 +27,7 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.os.UserManager;
 import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -277,9 +278,15 @@ public class MyDeviceInfoFragment extends DashboardFragment
         @Override
         public void setListening(boolean listening) {
             if (listening) {
-                mSummaryLoader.setSummary(this, DeviceModelPreferenceController.getDeviceModel());
+				getDeviceName();
             }
         }
+		
+		public void getDeviceName() {
+			final String deviceName = Settings.Global.getString(
+			        getContext().getContentResolver(), Settings.Global.DEVICE_NAME);
+			mSummaryLoader.setSummary(this, deviceName != null ? deviceName : DeviceModelPreferenceController.getDeviceModel());
+		}
     }
 
     public static final SummaryLoader.SummaryProviderFactory SUMMARY_PROVIDER_FACTORY
