7623c61ac0239caf1f91a89b71fa8c610beeb123
Chris <chris@cypheros.co>
2018-12-04 05:15:30 -0500

    Add preference for Display Mode
    Change-Id: I8453e4ef7586e795f7933eaa92ecad1983b46d8c

---
+++ src/com/android/settings/aoscp/display/DisplayModePreferenceController.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.aoscp.display;
+
+import android.content.Context;
+import android.util.Log;
+
+import aoscp.hardware.DeviceHardwareManager;
+
+import com.android.internal.annotations.VisibleForTesting;
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class DisplayModePreferenceController extends BasePreferenceController {
+    private static final String TAG = "DisplayModePreference";
+    private static final String KEY_DISPLAY_MODE = "display_mode";
+
+    private DeviceHardwareManager mHwManager;
+
+    public DisplayModePreferenceController(Context context) {
+        super(context, KEY_DISPLAY_MODE);
+		mHwManager = DeviceHardwareManager.getInstance(context);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mHwManager.isSupported(DeviceHardwareManager.FEATURE_DISPLAY_ENGINE) ? 
+		        AVAILABLE : DISABLED_FOR_USER;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        final int currentMode = mHwManager.getCurrentDisplayMode();
+        if (currentMode == 3) {
+            return mHwManager.getDisplayModeName(3);
+        }
+        if (currentMode == 2) {
+            return mHwManager.getDisplayModeName(2);
+        }
+        if (currentMode == 1) {
+            return mHwManager.getDisplayModeName(1);
+        }
+        return mHwManager.getDisplayModeName(0);
+    }
+}
